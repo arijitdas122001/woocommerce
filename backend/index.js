5a12c09e9739d1e1880c74ffc2a4685ebceab3f9
arijitdas122001 <108047442+arijitdas122001@users.noreply.github.com>
2023-08-27 22:05:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const Cart=require('./Routes/Cart');
 const Order=require('./Routes/Order');
 const Stripe=require('./Routes/Stripe');
 app.use(cors({
-    origin:["https://woocommerce-rho.vercel.app"],
+    origin:["https://woocommerce-pe5h.vercel.app"],
     methods:["POST","GET","PUT"],
     credentials:true
 }))
